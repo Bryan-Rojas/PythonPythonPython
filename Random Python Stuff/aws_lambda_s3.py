685ad4513d676097055b6dac1bfcb89b6e51f709
Bryan Rojas <bryroj14@gmail.com>
2019-06-14 20:45:18 -0700

    AWS boto3 added

--- Random Python Stuff/aws_lambda_s3.py
+++ Random Python Stuff/aws_lambda_s3.py
@@ -2,8 +2,7 @@ import boto3
 s3_client = boto3.client('s3')
 
 def my_handler(event, context):
-    message = 'Hello {} {}!'.format(event['first_name'], 
-                                    event['last_name'])  
+    message = 'Hello' + event['first_name'] + event['last_name']
     return { 
         'message' : message
     }  
